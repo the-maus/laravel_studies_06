f665f1711d88e4dd5e4a3820c0696649347f28c8
the-maus <msampaio.mail@gmail.com>
2026-01-28 18:07:43 -0300

    Models fetch data/methods

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,14 +3,86 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Models\TestModel;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        $results = TestModel::all()->toArray();
-        dd($results);
+        // fetch all data from products
+        // $results = Product::all(); // SELECT * FROM products
+
+        // foreach ($results as $product) {
+        //     echo '<br>';
+        //     echo $product->product_name;
+        // }
+
+        // echo $results[0]->product_name;
+
+
+        // fetch all data as associative array
+        // $results = Product::all()->toArray();
+        // $this->showData($results);
+
+        // get results as array of stdClass objects
+        // $results = $this->arrayOfObjects(Product::all()->toArray());
+
+
+        // fetch products ordered by name
+        // $results = Product::orderBy('product_name')
+        //     ->get()
+        //     ->toArray();
+
+
+        // get the first 3 products
+        // $results = Product::limit(3)->get()->toArray();
+
+        // find product by id
+        // $results = Product::find(10)->toArray();
+
+        // where clause
+        // $results = Product::where('price', '>=', 70)->get()->toArray();
+
+        // get only first result
+        // $results = Product::where('price', '>=', 70)->first()->toArray();
+
+        // get only first element if exists, otherwise return empty array
+        // $results = Product::where('price', '>=', 190)
+        //     ->firstOr(function () {
+        //         return [];
+        //     });
+
+        
+        $product = Product::find(10);
+        echo $product->price;
+        echo '<br>';
+
+        $product->price = 200;
+        echo $product->price;
+        echo '<br>'; 
+
+        $product->refresh(); // get original data 
+        echo $product->price;
+        echo '<br>'; 
+
+
+        // $this->showData($results);
+    }
+
+    private function showData($data)
+    {
+        echo '<pre>';
+        print_r($data);
+    }
+
+    private function arrayOfObjects($data)
+    {
+        $tmp = [];
+
+        foreach ($data as $key => $value) {
+            $tmp[] = (object) $value;
+        }
+
+        return $tmp;
     }
 }

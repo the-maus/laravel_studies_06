0002d8fb60695d85e13ba0876b89715abf39c222
the-maus <msampaio.mail@gmail.com>
2026-01-28 23:15:37 -0300

    Inserting data using Eloquent Models

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,100 +4,50 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class MainController extends Controller
 {
     public function index()
     {
-        // fetch all data from products
-        // $results = Product::all(); // SELECT * FROM products
+        // INSERT INTO products(product_name, price) VALUES ('New Product', 50)
+        // automatically inserts values for created_at/updated_at
+        // $new_product = new Product();
+        // $new_product->product_name = 'New Product';
+        // $new_product->price = 50;
+        // $new_product->save();
 
-        // foreach ($results as $product) {
-        //     echo '<br>';
-        //     echo $product->product_name;
-        // }
-
-        // echo $results[0]->product_name;
-
-
-        // fetch all data as associative array
-        // $results = Product::all()->toArray();
-        // $this->showData($results);
-
-        // get results as array of stdClass objects
-        // $results = $this->arrayOfObjects(Product::all()->toArray());
+        // INSERT INTO products(product_name, price) VALUES ('Other Product', 160)
+        // automatically inserts values for created_at/updated_at
+        // needs to set ($fillable = ['product_name', 'price']) on Model
+        // Product::create([
+        //     'product_name' => 'Other Product',
+        //     'price' => 160
+        // ]);
 
 
-        // fetch products ordered by name
-        // $results = Product::orderBy('product_name')
-        //     ->get()
-        //     ->toArray();
-
-
-        // get the first 3 products
-        // $results = Product::limit(3)->get()->toArray();
-
-        // find product by id
-        // $results = Product::find(10)->toArray();
-
-        // where clause
-        // $results = Product::where('price', '>=', 70)->get()->toArray();
-
-        // get only first result
-        // $results = Product::where('price', '>=', 70)->first()->toArray();
-
-        // get only first element if exists, otherwise return empty array
-        // $results = Product::where('price', '>=', 190)
-        //     ->firstOr(function () {
-        //         return [];
-        //     });
-
-        // $product = Product::find(10);
-        // echo $product->price;
-        // echo '<br>';
-
-        // $product->price = 200;
-        // echo $product->price;
-        // echo '<br>'; 
-
-        // $product->refresh(); // get original data 
-        // echo $product->price;
-        // echo '<br>'; 
-
-        // $this->showData($results);
-
-        // $product = Product::find(10);
-        // echo $product->product_name . '<br>';
-
-        // $product = Product::where('price', '>=', 70)->first();
-        // echo $product->product_name . ' costs $' . $product->price . '<br>';
-
-        // $product = Product::firstWhere('price', '>=', 60);
-        // echo $product->product_name . ' costs $' . $product->price . '<br>';
-
-        // $product = Product::findOr(110, function () {
-        //     echo 'Product not found';
-        // });
-        // if ($product) echo $product->product_name . ' costs $' . $product->price . '<br>';
-
-        // $product = Product::findOrFail(120); // returns default laravel 404 page in case it doesnt exist
-        // echo $product->product_name . ' costs $' . $product->price . '<br>';
-
-        $total_products = Product::count();
-        $product_max_price = Product::max('price');
-        $product_min_price = Product::min('price');
-        $product_avg_price = Product::avg('price');
-        $product_sum_price = Product::sum('price');
-
-        $results = [
-            'total_products'    => $total_products,
-            'product_max_price' => $product_max_price,
-            'product_min_price' => $product_min_price,
-            'product_avg_price' => $product_avg_price,
-            'product_sum_price' => $product_sum_price
-        ];
-
-        $this->showData($results);
+        // mass insertion
+        // doesnt insert values for created_at/updated_at automatically
+        Product::insert([
+            [
+                'product_name' => 'Product 6',
+                'price' => 40,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'product_name' => 'Product 5',
+                'price' => 60,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'product_name' => 'Product 4',
+                'price' => 50,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+        ]);
     }
 
     private function showData($data)

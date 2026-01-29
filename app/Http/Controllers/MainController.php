e16da447c0c9fad4ac8dff29d63947e25231cce5
the-maus <msampaio.mail@gmail.com>
2026-01-28 23:27:07 -0300

    Updating data with Eloquent models

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,44 +10,25 @@ class MainController extends Controller
 {
     public function index()
     {
-        // INSERT INTO products(product_name, price) VALUES ('New Product', 50)
-        // automatically inserts values for created_at/updated_at
-        // $new_product = new Product();
-        // $new_product->product_name = 'New Product';
-        // $new_product->price = 50;
-        // $new_product->save();
+        // // SELECT * FROM products WHERE id = 10
+        // $product = Product::find(10); 
+        // // UPDATE products SET name = 'Changed product', price = 10 WHERE id = 10
+        // // automatically updates updated_at column
+        // $product->product_name = 'Changed product';
+        // $product->price = 10;
+        // $product->save();
 
-        // INSERT INTO products(product_name, price) VALUES ('Other Product', 160)
-        // automatically inserts values for created_at/updated_at
-        // needs to set ($fillable = ['product_name', 'price']) on Model
-        // Product::create([
-        //     'product_name' => 'Other Product',
-        //     'price' => 160
-        // ]);
+        // // mass update
+        // Product::where('price', '<=', 10)
+        //         ->update([
+        //             'price' => 150
+        //         ]);
 
-
-        // mass insertion
-        // doesnt insert values for created_at/updated_at automatically
-        Product::insert([
-            [
-                'product_name' => 'Product 6',
-                'price' => 40,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'product_name' => 'Product 5',
-                'price' => 60,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'product_name' => 'Product 4',
-                'price' => 50,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-        ]);
+        // update (if it exists) or create
+        Product::updateOrCreate(
+            ['product_name' => 'Xarope'], // attributes to search by
+            ['price' => 25] // values to update/create
+        );
     }
 
     private function showData($data)
